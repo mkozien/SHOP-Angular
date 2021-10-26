2390dc8fd536d8410e243067e6dba5d33c8058f8
mkozien <marek.kozien@gmail.com>
2021-10-26 16:33:09 +0200

    product removal and sum added

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -10,14 +10,21 @@ import {BasketService} from "../basket.service";
 export class BasketComponent implements OnInit {
 
   items: Product[] = [];
+  price: number;
 
   constructor(private basketService: BasketService) {
-    this.items = this.basketService.getProducts()
+    this.items = this.basketService.getProducts();
+    this.price = this.countSum();
+  }
+
+  countSum() {
+    return this.items.map(({price}) => price).reduce((sum, current) => sum + current, 0);
   }
 
   removeItem(itemId: number){
     this.basketService.removeProduct(itemId);
     this.items = this.items.filter(item => item.id !== itemId);
+    this.price = this.countSum();
   }
 
   removeAllItems() {
