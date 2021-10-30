052f40eaa3e99b16577b0fd90b982a7f270d7324
mkozien <marek.kozien@gmail.com>
2021-10-30 13:33:53 +0200

    in progress 3

--- src/app/basket.service.ts
+++ src/app/basket.service.ts
@@ -14,16 +14,16 @@ export class BasketService {
     this.items.push(productInBasket)
   }
 
-  // getProducts() {
-  //   return this.items;
-  // }
-  //
-  // removeProduct(productId: number) {
-  //   this.items = this.items.filter(item => item.id !== productId);
-  // }
-  //
-  // clearBasket(){
-  //   this.items = [];
-  //   return this.items;
-  // }
+  getProducts() {
+    return this.items;
+  }
+
+  removeProduct(productId: number) {
+    this.items = this.items.filter(item => item.id !== productId);
+  }
+
+  clearBasket(){
+    this.items = [];
+    return this.items;
+  }
 }
